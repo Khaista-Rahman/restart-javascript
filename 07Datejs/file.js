bd969fe07dc9e3657c3087abdcd98d84aebe54b1
Khaista-Rahman <Khaistarahman.cs@gmail.com>
2024-04-28 17:27:01 +0000

    in this part i learn Array in javascript in latest form

--- 07Datejs/file.js
+++ 07Datejs/file.js
@@ -1,24 +1,24 @@
 let newDate = new Date();
-// console.log(newDate);
-// console.log(newDate.toLocaleString());
-// console.log(newDate.getHours());
-// console.log(newDate.getMinutes());
-// console.log(newDate.getMilliseconds());
+console.log(newDate);
+console.log(newDate.toLocaleString());
+console.log(newDate.getHours());
+console.log(newDate.getMinutes());
+console.log(newDate.getMilliseconds());
 
 let updateDate = new Date("2024 , 3 , 4");
-// console.log(updateDate.toLocaleString());
+console.log(updateDate.toLocaleString());
 
 let seconds = new Date(2030,4,23);
-// console.log(seconds.toLocaleString());
-// console.log(seconds.getFullYear());
-// console.log(seconds.getMonth());
-// console.log(seconds.getDate());
+console.log(seconds.toLocaleString());
+console.log(seconds.getFullYear());
+console.log(seconds.getMonth());
+console.log(seconds.getDate());
 
 let nowDate = new Date();
-// console.log(nowDate.getUTCDate());
+console.log(nowDate.getUTCDate());
 
-let lunchTime = new Date('2, 11, 2025');
-const timer = (setTimeout((lunchTime) => {
-  lunchTime;
-}, 4000));
-console.log(timer);
+
+let newTime = new Date("2,5,1990");
+console.log(newTime.getDate());
+console.log(newTime.getMonth());
+console.log(newTime.getFullYear());
