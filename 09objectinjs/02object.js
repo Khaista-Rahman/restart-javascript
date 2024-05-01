10fb6a6b572bc09081a8cae94c004921a39bef65
Khaista-Rahman <Khaistarahman.cs@gmail.com>
2024-05-01 18:02:42 +0000

    learn function from youtube

--- 09objectinjs/02object.js
+++ 09objectinjs/02object.js
@@ -1,102 +1,102 @@
-// // singleton is simply create in this method.
+// singleton is simply create in this method.
 
-// // const User1 = new Object();
-// const User1 = {}
-// User1.Name = "khaista rahman";
-// User1.Age = 20;
-// User1.IsLoggedIn = true;
+// const User1 = new Object();
+const User1 = {}
+User1.Name = "khaista rahman";
+User1.Age = 20;
+User1.IsLoggedIn = true;
 
-// // console.log(User1)
-
-
-// const User2 ={
-//   email: "rahamn@gmail.com",
-//   User:{
-//     Full_Name:{
-//       Name: "shahid zada",// this is called nested object means inside the object of object.
-//       Age: 22
-//     }
-//   }
-// };
-// console.log(User2.User.Full_Name.Age);
-
-// const regularCustomar1 ={
-//   Name : "khaista Rahman",
-//   age: 30,
-//   email: 'rahman@gamil.com'
-// }
-
-// const regularCustomar2 = {
-//   Name: "shahid zada",
-//   age : 20,
-//   email: "zada@gmail.com"
-// }
-// const obj4 = Object.assign(regularCustomar1,regularCustomar2);
-// console.log(obj4);
+// console.log(User1)
 
 
-// const combine1 = {...regularCustomar1,...regularCustomar2};
-// console.log(combine1);
+const User2 ={
+  email: "rahamn@gmail.com",
+  User:{
+    Full_Name:{
+      Name: "shahid zada",// this is called nested object means inside the object of object.
+      Age: 22
+    }
+  }
+};
+console.log(User2.User.Full_Name.Age);
+
+const regularCustomar1 ={
+  Name : "khaista Rahman",
+  age: 30,
+  email: 'rahman@gamil.com'
+}
+
+const regularCustomar2 = {
+  Name: "shahid zada",
+  age : 20,
+  email: "zada@gmail.com"
+}
+const obj4 = Object.assign(regularCustomar1,regularCustomar2);
+console.log(obj4);
 
 
-// const obj1 = {1: "a" , 2: "b"};
-// const obj2 = {3: "a" , 4: "b"};
-// const obj3 = Object.assign(obj1,obj2);
-// console.log(obj3);
-
-// const khaista = {
-//   full_Name: "khaista rahman",
-//   full_email: "rahman@gmail.com",
-//   phone: null
-// }
-// const shahid = {
-//   Name: "shahid zada",
-//   email: "zada@gmail.com",
-//   contact: null
-// }
-
-// const combine = {...khaista,...shahid}
-// console.log(combine);
-
-// const tinderuser = {
-//  user1: [
-//     full_Name,"khaista",
-//     email, "h@gamil.com"
-//   ],
-//   user2: [
-//     full_Name,"khaista",
-//     email, "h@gamil.com"
-//   ],
-//   user3: [
-//     full_Name,"khaista",
-//     email, "h@gamil.com"
-//   ],
-// }
+const combine1 = {...regularCustomar1,...regularCustomar2};
+console.log(combine1);
 
 
-// console.log(Object.keys(User1));
-// console.log(Object.values(User1));
-// console.log(Object.entries(User1));
-// // console.log(User1.hasOwnproperty("isloogini"))
+const obj1 = {1: "a" , 2: "b"};
+const obj2 = {3: "a" , 4: "b"};
+const obj3 = Object.assign(obj1,obj2);
+console.log(obj3);
+
+const khaista = {
+  full_Name: "khaista rahman",
+  full_email: "rahman@gmail.com",
+  phone: null
+}
+const shahid = {
+  Name: "shahid zada",
+  email: "zada@gmail.com",
+  contact: null
+}
+
+const combine = {...khaista,...shahid}
+console.log(combine);
+
+const tinderuser = {
+ user1: [
+    full_Name,"khaista",
+    email, "h@gamil.com"
+  ],
+  user2: [
+    full_Name,"khaista",
+    email, "h@gamil.com"
+  ],
+  user3: [
+    full_Name,"khaista",
+    email, "h@gamil.com"
+  ],
+}
+
+
+console.log(Object.keys(User1));
+console.log(Object.values(User1));
+console.log(Object.entries(User1));
+// console.log(User1.hasOwnproperty("isloogini"))
 
 
 
-// const users = [
-//   {
-//     email: "ra@gmail.com",
-//     contact: null
-//   },
-//   {
-//     email: "ra@shs.com",
-//     contact: null
-//   },
-//   {
-//     email: "ra@23423.com",
-//     contact: "null"
-//   }
-// ];
-// console.log(users[1].email);
-// console.log(users[2].email);
+const users = [
+  {
+    email: "ra@gmail.com",
+    contact: null
+  },
+  {
+    email: "ra@shs.com",
+    contact: null
+  },
+  {
+    email: "ra@23423.com",
+    contact: "null"
+  }
+];
+console.log(users[1].email);
+console.log(users[2].email);
 
 
 
@@ -117,3 +117,23 @@ console.log(Teacher);
 
 
 // actullay this process is called distructering and this is very usefully in react . so basically we can do distucturing in array and object . but the array disturctering is very rearly .
+
+
+// ++++++++++++++++++++++++++++++++ JASON API ++++++++++++++++++++++++++++++++++++
+// api structure.
+
+// {
+//   "name" : "khaista rahman",
+//   "age" : 20,
+//   "Contact" : null ,
+// }
+
+
+// some time api give us the data in form of array in the inside of array thats alot of object . so basically we will learn more about of api but this is the structure of api .
+
+
+// [
+//   {},
+//   {},
+//   {}
+// ]
---
+++ functioninjs/01function.js
@@ -0,0 +1,69 @@
+function myfunction(){
+  console.log("my name is khaista rahman");
+}
+//myfunction// simply this is called refreance of function not excute.
+myfunction()// this called callback of function.
+
+// add two numbers in function .
+
+function addTwoNumbers(num1, num2){
+     console.log(num1 + num2)
+}
+addTwoNumbers()// the expected result will be NAN means not a number because we did'nt give arguments in callback/.
+
+addTwoNumbers(3 ,5)// the expected result is 8 .
+
+// here is one more concept of interpulation in function so will do that.
+
+function addNumbers(num1, num2){
+  console.log(`The Result is : ${num1 + num2}`)
+}
+addNumbers(22,44)
+
+
+// The Return concept .
+
+function myfun(userName){
+    return `${userName} is logged in`;// after this return you anthing code that will be not excute . we should be try this hehehe
+    console.log("shahid zada")
+
+}
+console.log(myfun())// the expected excute will be undefined.
+console.log(myfun("shahid zada"));
+
+// i did'nt learn about if else but now we will practice this in function to learn will functions .
+
+
+function userFunction(userName){
+    if(!userName){
+      console.log("please enter a user Name");
+    }
+    return userName;
+}
+
+console.log(userFunction());
+
+// my practice .
+
+const myArray = ["shahid", "khaista", "zain"];
+function myfunction2(userName){
+  if(!userName){// ! this mean not .
+    console.log(`my Name is ${myArray[2]}`);
+  }
+  return `my name is ${userName}`
+}
+console.log(myfunction2("khaista rahman"));
+myfunction2();
+
+const myObject ={
+  Name : "saqib",
+  age: 23,
+  email: null,
+  address: "pakistan swat"
+}
+
+function myfunction3(){
+  console.log(`my name is ${myObject.Name} and my age is ${myObject.age} and my email is ${myObject.email} and i belongs to ${myObject.address}`)
+}
+myfunction3()
+// here we learn about functions and how to inject the array or object in functions . 
