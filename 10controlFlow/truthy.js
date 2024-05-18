bbc56c27f54d285019a54ecc100ef6ebbd30be74
Khaista-Rahman <Khaistarahman.cs@gmail.com>
2024-05-18 18:29:31 +0000

    for loop

--- 10controlFlow/truthy.js
+++ 10controlFlow/truthy.js
@@ -1,11 +1,31 @@
+
+// falsey values
+// flase , 0 , -0 , bigint , 0n , "" , null , undefined , NaN
+
+// anthing you code in stringe this will be truthy . even space will be.
+// truthy values
+// "0" , "false" , " " , [] , {} , function(){}
+
+// here is the examples of truthy and falsey
+
+// let a = "";
+let b = "0";
+if (b) {
+  console.log("this is truthy")
+} else {
+  console.log("this is falsey");
+}
+// you can practice in this to find truthy values and falsey values
+
 const poo = null ?? 12;
 console.log(poo)
 
 const pooo = undefined ?? 34;
 console.log(pooo)
 
+let num = undefined ?? 13 ?? 323;
+console.log(num)
+// if there is alot of number or values this will be excute what the find first.
+
 // ?? this is called nullish coalscing operator .
 // this is always execute the right side oprand otherwise the left side oprand is null or undefined .
-
-const user = {}
-console.log(user)
---
+++ 11LOOPS/one.js
@@ -0,0 +1,44 @@
+// for()
+
+for (let j = 0; j <=10; j++) {
+  // console.log(j)
+}
+
+for (let i = 0; i <=10; i++) {
+  // console.log(`Table of: ${i}`)
+  for (let j = 0; j <=10; j++) {
+    // console.log(i + " * " +j + " = " + i*j);
+  }
+  
+}
+
+for (let n = 0; n <= 100; n++) {
+  // console.log(`${n} i love you`);
+  
+}
+
+let myarr = ["khaista" , "shahid" , "zain" , "papa"];
+
+if (myarr[3]) {
+  for (let i = 0; i <=10; i++) {
+    // console.log(myarr[3]);
+    
+  }
+}else{
+  // console.log("not here")
+}
+
+let myobj = {
+  name: "zain khan",
+  age: 12,
+  email: "zain@gmail.com",
+  contact: 34647829754
+}
+if (myobj.contact) {
+  for (let u = 1; u <=10 ; u++) {
+    console.log(myobj)
+    
+  }
+} else {
+  console.log("phone number is not here")
+}
